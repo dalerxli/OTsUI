ea531a9e6f1e92b8f93ac47c88c6bb3c367385d3
Ettore Landini <ettore.landini@outlook.it>
2015-05-07 12:43:18 +0200

    001 - Just a skeleton

---
+++ OTsUI_launcher.py
@@ -0,0 +1,11 @@
+from tools import *
+from epz import ch_fw as fw
+from epz import ch_hw as hw
+from epz import epdspic as pic
+
+from dialogs import *
+
+if __name__ == '__main__':
+    
+    print('Work in progress...')
+    
