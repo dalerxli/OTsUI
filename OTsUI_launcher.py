0d2ace7adaadca958ea97874e1eac27b5d639856
Ettore Landini <ettore.landini@outlook.it>
2015-05-15 17:49:23 +0200

    001bis - Minor changes

--- OTsUI_launcher.py
+++ OTsUI_launcher.py
@@ -1,11 +1,14 @@
-from tools import *
 from epz import ch_fw as fw
 from epz import ch_hw as hw
 from epz import epdspic as pic
+from PyQt5.QtWidgets import QApplication
+import sys
 
 from dialogs import *
 
 if __name__ == '__main__':
+    
+    app = Qapplication(sys.argv)
     
     print('Work in progress...')
     
